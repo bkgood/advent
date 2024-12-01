221c606b4b627ce6b75277291c562139899d356f
William Good <bkgood@gmail.com>
2024-12-01 13:34:50 +0100

    2025 begins

---
+++ 2024/day-01/day.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+(require racket/treelist)
+(require racket/string)
+
+(define ex #<<>>
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+>>
+  )
+
+(define (string-empty? s) (zero? (string-length s)))
+
+(define (parse in)
+  (define lines
+    (for/list ([line (in-lines in 'any)]
+               #:do [(define trimmed (string-trim line))]
+               #:unless (string-empty? trimmed))
+      (for/list ([num (string-split line)])
+        (string->number num))))
+  (for/fold ([lists (cons empty-treelist empty-treelist)])
+            ([l (in-list lines)])
+    (cons
+     (treelist-add (car lists) (car l))
+     (treelist-add (cdr lists) (cadr l)))))
+
+(define (dists cols)
+  (for/sum ([c1 (treelist-sort (car cols) <)]
+            [c2 (treelist-sort (cdr cols) <)])
+    (abs (- c1 c2))))
+
+(define (pt-1 in)
+  (define cols (parse in))
+  (dists cols))
+
+(define (pt-2 in)
+  (define cols (parse in))
+  (define histo
+    (for/fold ([m (make-immutable-hash)])
+              ([x (in-treelist (cdr cols))])
+      (hash-update m x add1 (thunk 0))))
+  (for/sum ([x (in-treelist (car cols))])
+    (* x (hash-ref histo x 0))))
+
+(pt-1 (open-input-string ex))
+(call-with-input-file "in" pt-1 #:mode 'text)
+
+(pt-2 (open-input-string ex))
+(call-with-input-file "in" pt-2 #:mode 'text)
