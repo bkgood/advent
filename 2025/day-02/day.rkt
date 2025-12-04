86bda34dda7730d1a2b48b7fba1ea8ff64e2a02c
William Good <bkgood@gmail.com>
2025-12-04 20:53:18 +0100

    load faster with racket/base and necessary require

--- 2025/day-02/day.rkt
+++ 2025/day-02/day.rkt
@@ -1,4 +1,12 @@
-#lang racket
+#lang racket/base
+
+(require (only-in racket/string
+                  string-split)
+         (only-in racket/list
+                  second
+                  third)
+         (only-in racket/port
+                  call-with-input-string))
 
 (define ex #<<>>
 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
